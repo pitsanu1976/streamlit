8f0db8f67651d7204c088f5562eb571450870065
pitsanu1976 <pitsanu.1976@gmail.com>
2023-06-13 23:43:54 +0700

    initialize

---
+++ home.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title='Multipage App',
+    page_icon="Hi"
+)
+
+st.title('Main Page')
+st.sidebar.success('select a page above')
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"]=""
+    
+my_input = st.text_input('input a text here',st.session_state["my_input"])
+submit = st.button('submit')
+if submit:
+    st.session_state["my_input"]=my_input
+    st.write('You have entered:',my_input)
---
+++ pages/2_project.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title('Project')
+
+st.write('you enter',st.session_state['my_input'])
