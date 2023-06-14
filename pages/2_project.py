78bb6f285cf727aef2f1688a7a685760d2e8b13a
pitsanu1976 <71583393+pitsanu1976@users.noreply.github.com>
2023-06-14 14:03:32 +0700

    add other package

--- pages/2_project.py
+++ pages/2_project.py
@@ -1,7 +1,14 @@
 import streamlit as st 
 
+import pandas as pd
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+data = pd.DataFrame(a,columns=['a','b','c'])
+
 st.title('Project')
 
 st.header('header for project')
 
 st.write('you enter',st.session_state['my_input'])
+
+st.write(data)
