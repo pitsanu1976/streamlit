908b8d19fc7eb86087b44a703bf2fb6e04788121
pitsanu1976 <71583393+pitsanu1976@users.noreply.github.com>
2023-06-14 14:15:19 +0700

    edit data

--- pages/2_project.py
+++ pages/2_project.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 a = [[1,2,3],[4,5,6],[7,8,9]]
-b = [[1,2,3],[4,5,6],[7,8,9]]
+b = [[1,2,3],[4,5,6],[10,11,12]]
 data1 = pd.DataFrame(a,columns=['a','b','c'])
 data2 = pd.DataFrame(b,columns=['a','b','c'])
 st.title('Project')
