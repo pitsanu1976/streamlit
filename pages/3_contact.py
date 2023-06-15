88f401d4fe900069ffd14b635f5ef0baa2a4754d
pitsanu1976 <71583393+pitsanu1976@users.noreply.github.com>
2023-06-15 12:37:11 +0700

    delete ai

--- pages/3_contact.py
+++ pages/3_contact.py
@@ -1,18 +1,3 @@
-import streamlit as st 
-import openai
+import streamlit as st
+
 st.title('contact')
-openai.api_key = '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'
-
-def openai_chat(prompt):
-    completions = openai.Completion.create(
-        engine = 'text-davinci-003',
-        prompt = prompt,
-        max_tokens = 1024,
-        n = 1,
-        temperature = 0.8
-    )
-    message = completions.choices[0].text 
-    return message.strip()
-
-prompt_text = st.input_text('enter prompt:')
-st.write(openai_chat(prompt_text))
