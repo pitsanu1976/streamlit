bf728c19cc36bf120b38a03d8f70447858fed3c8
pitsanu1976 <71583393+pitsanu1976@users.noreply.github.com>
2023-06-15 12:28:44 +0700

    edit key

--- home.py
+++ home.py
@@ -1,20 +1,42 @@
 import streamlit as st 
+from streamlit_chat import message
+import openai
+# openai.api_key = st.secrets['pass']
+openai.api_key = "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"
 
-st.set_page_config(
-    page_title='Multipage App',
-    page_icon="Hi"
-)
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = 'text-davinci-003',
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.8
+    )
+    message = completions.choices[0].text 
+    return message
 
-st.title('Main Page')
-st.header('this is the home page')
-st.subheader('Test, this is subheader')
-st.sidebar.success('select a page above')
+st.title('ChatBot')
 
-if "my_input" not in st.session_state:
-    st.session_state["my_input"]=""
+#store interaction
+if 'generated' not in st.session_state:
+    st.session_state['generated']=[]
+if 'past' not in st.session_state:
+    st.session_state['past']=[]
     
-my_input = st.text_input('input a text here',st.session_state["my_input"])
-submit = st.button('submit')
-if submit:
-    st.session_state["my_input"]=my_input
-    st.write('You have entered:',my_input)
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?",key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+    
+if st.session_state['generated']:
+    
+    for i in range(len(st.session_state['generated'])-1,-1,-1):
+        message(st.session_state["generated"][i],key=str(i))
+        message(st.session_state['past'][i],is_user=True,key=str(i)+'_user')
